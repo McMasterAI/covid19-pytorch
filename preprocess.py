f9820fdb26bf32eb2b6f0ba303140f1abfef3ad3
Connor Sung Czarnuch <connor.czarnuch@icloud.com>
2020-08-07 15:47:08 -0400

    add __main__ clause

--- preprocess.py
+++ preprocess.py
@@ -89,9 +89,7 @@ def plot(unique_elements, counts_elements):
 
 def main():
     url = "https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv"
-    save_location = (
-        "/home/connorsungczarnuch/projects/covid19-pytorch/temp/conposcovidloc.csv"
-    )
+    save_location = os.getcwd() + "/temp/conposcovidloc.csv"
 
     download_csv(url, save_location)
     unique, counts = process_csv(save_location)
@@ -99,3 +97,7 @@ def main():
     for i in range(len(data_array[0])):
         print(data_array[0][i], data_array[1][i])
     plot(data_array[0], data_array[1])
+
+
+if __name__ == "__main__":
+    main()
