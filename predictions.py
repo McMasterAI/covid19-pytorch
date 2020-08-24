c0efe4d0f7634e616d489a1ff311d5c164e686c6
jaymody <jaykmody@gmail.com>
2020-08-24 13:51:35 -0400

    use os.path.join over string concat as it's a safer option

--- predictions.py
+++ predictions.py
@@ -128,9 +128,9 @@ def convert_json(save_location, locations_dict, prediction_dates=[]):
 
 def main():
     url = "https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv"
-    csv_location = os.getcwd() + "/temp/conposcovidloc.csv"
-    model_location = os.getcwd() + "/temp/model.pt"
-    model_base_location = os.getcwd() + "/temp/"
+    csv_location = os.path.join(os.getcwd(), "temp/conposcovidloc.csv")
+    model_location = os.path.join(os.getcwd(), "temp/model.pt")
+    model_base_location = os.path.join(os.getcwd(), "temp/")
     json_location = model_base_location
 
     # get data and create inout sequences
